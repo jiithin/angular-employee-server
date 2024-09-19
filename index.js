bce7c1e8a15a28b66a7126c6f1909efab180567b
jiithin <jithijithin9@mail.com>
2024-09-19 13:25:33 +0530

    employee management app server

---
+++ index.js
@@ -0,0 +1,13 @@
+const jsonServer = require('json-server')
+const empServer = jsonServer.create()
+const middleWare = jsonServer.defaults()
+const router = jsonServer.router('db.json')
+const PORT = 3000 || process.env.PORT
+
+
+empServer.use(middleWare)
+empServer.use(router)
+
+empServer.listen(PORT,()=>{
+    console.log(`empServer started at PORT: ${PORT}.. waitnig for client request`);
+})
